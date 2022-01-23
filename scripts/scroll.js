9dc6d339e4f3ac8fc17666b495c3a6f665f0d327
sanzhar <62831632+Sanzhar-MAK@users.noreply.github.com>
2022-01-23 22:34:10 +0600

    Add files via upload

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,34 +1,28 @@
-const links = document.querySelectorAll('.header-menu__item a');
-const allCharacteristics = document.querySelector('.card-details__link-characteristics');
-seamless.polyfill();
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        const target = event.target;
+const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a');
+    const allCharacteristics = document.querySelector('.card-details__link-characteristics');
+    const newArray = [...links, allCharacteristics];
+    seamless.polyfill();
 
-        const id = element.getAttribute('href').substring(1);
-        const section = document.getElementById(id)
+    newArray.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = event.target;
 
-        if (section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: "smooth",
-                block: "start",
-            });
-        } else {
-            console.log('Null')
-        }
+            const id = element.getAttribute('href').substring(1);
+            const section = document.getElementById(id)
+
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: "smooth",
+                    block: "start",
+                });
+            } else {
+                console.log('Null')
+            }
+        })
     })
-})
 
-allCharacteristics.addEventListener('click', (event) => {
-    event.preventDefault();
-    const target = event.target
-    const idAll = target.getAttribute('href').substring(1);
-    const sectionAll = document.getElementById(idAll)
+}
 
-    seamless.elementScrollIntoView(sectionAll, {
-        behavior: "smooth",
-        block: "start",
-    });
-
-})
+scrollFunc()
