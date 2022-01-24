bf9955218fba5929cfe5feabb9a4898e63bc98a5
sanzhar <62831632+Sanzhar-MAK@users.noreply.github.com>
2022-01-24 23:15:50 +0600

    Update getData.js

--- scripts/getData.js
+++ scripts/getData.js
@@ -40,7 +40,7 @@ const getData = () => {
     }
 
     const getGoods = () => {
-        fetch('cross-sell-dbase/dbase.json')
+        fetch('https://iphone-f31ee-default-rtdb.firebaseio.com/db.json')
             .then((response) => {
                 if (response.ok) {
                     return response.json()
